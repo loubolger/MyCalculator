f5958ca92a75f21e56060c54ca36da2a6d4ce115
loubolger <arkjune2011>
2013-04-11 13:21:23 +0100

    commit

---
+++ MyCalculator/src/ie/gcd/maths/ScientificCalculator.java
@@ -0,0 +1,16 @@
+package ie.gcd.maths;
+
+public class ScientificCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static int add(int x, int y){
+		return x-y;
+	}
+
+}
